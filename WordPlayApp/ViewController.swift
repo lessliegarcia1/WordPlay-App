79d5f43079cf0800982f38def983abac2b9ee443
Lesslie Garcia <student@lgarcia1.local>
2016-12-08 11:15:22 -0600

    Initial Commit

---
+++ WordPlayApp/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  WordPlayApp
+//
+//  Created by Lesslie Garcia on 12/2/16.
+//  Copyright © 2016 Lesslie Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+            }
+    
+    @IBOutlet weak var firstWordBank: UITextField!
+    
+    @IBOutlet weak var secondWordBank: UITextField!
+    
+    @IBOutlet weak var thirdWordBank: UITextField!
+    
+    @IBOutlet weak var fourthWordBank: UITextField!
+    
+    @IBOutlet weak var fifthWordBank: UITextField!
+    
+    @IBOutlet weak var sixthWordBank: UITextField!
+    
+    @IBOutlet weak var seventhWordBank: UITextField!
+    
+    @IBOutlet weak var eigthWordBank: UITextField!
+    
+
+    
+    
+    
+    
+    
+    
+    
+}
